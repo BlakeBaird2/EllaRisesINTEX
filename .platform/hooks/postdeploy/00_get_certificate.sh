097e8c0e235a50000d9012f8c85df04fbfd68395
jigby5 <johnhigby5@gmail.com>
2025-12-02 12:59:35 -0700

    Adding https certificate code

---
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# Place in .platform/hooks/postdeploy directory
+sudo certbot -n -d http://vanillaintex.us-east-2.elasticbeanstalk.com/ --nginx --agree-tos --email johnhigby5@gmail.com
